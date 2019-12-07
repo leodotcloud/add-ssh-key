760a21a6b8987b9689854e3e3d3fb68aa617b346
Murali Paluru <leodotcloud@gmail.com>
2019-12-07 15:05:42 -0800

    adding newline check

--- package/add_ssh_key.sh
+++ package/add_ssh_key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "${SSH_KEY_TO_ADD}" == "" ]]; then
-    echo "Error: no key specified to add, delete and run again"
+  echo "Error: no key specified to add, delete and run again"
 fi
 
 MAPPED_USER_HOME="/tmp/user"
@@ -12,23 +12,28 @@ USE_UID=$(stat -c '%u' ${MAPPED_USER_HOME})
 USE_GID=$(stat -c '%g' ${MAPPED_USER_HOME})
 
 if [ ! -d  ${MAPPED_USER_SSH_FOLDER} ]; then
-    echo ".ssh folder doesn't exist, creating it."
-    gosu ${USE_UID}:${USE_GID} mkdir ${MAPPED_USER_SSH_FOLDER}
-    gosu ${USE_UID}:${USE_GID} chmod 700 ${MAPPED_USER_SSH_FOLDER}
+  echo ".ssh folder doesn't exist, creating it."
+  gosu ${USE_UID}:${USE_GID} mkdir ${MAPPED_USER_SSH_FOLDER}
+  gosu ${USE_UID}:${USE_GID} chmod 700 ${MAPPED_USER_SSH_FOLDER}
 fi
 
 if [ ! -f  ${AUTH_KEYS_FILE} ]; then
-    echo "authorized_keys file doesn't exist, creating it"
-    gosu ${USE_UID}:${USE_GID} touch ${AUTH_KEYS_FILE}
-    gosu ${USE_UID}:${USE_GID} chmod 644 ${AUTH_KEYS_FILE}
+  echo "authorized_keys file doesn't exist, creating it"
+  gosu ${USE_UID}:${USE_GID} touch ${AUTH_KEYS_FILE}
+  gosu ${USE_UID}:${USE_GID} chmod 644 ${AUTH_KEYS_FILE}
 fi
 
 grep "${SSH_KEY_TO_ADD}" ${AUTH_KEYS_FILE} > /dev/null
 if [[ $? -ne 0 ]]; then
-    echo "Adding SSH key"
-    gosu ${USE_UID}:${USE_GID} echo ${SSH_KEY_TO_ADD} >> ${AUTH_KEYS_FILE}
+  lastchar=$(tail -c 1 ${AUTH_KEYS_FILE})
+  if [[ "${lastchart}" != "" ]]; then
+    echo "Adding new line character"
+    gosu ${USE_UID}:${USE_GID} echo "" >> ${AUTH_KEYS_FILE}
+  fi
+  echo "Adding SSH key"
+  gosu ${USE_UID}:${USE_GID} echo ${SSH_KEY_TO_ADD} >> ${AUTH_KEYS_FILE}
 else
-    echo "Key already added"
+  echo "Key already added"
 fi
 
 echo "Going to sleep forever, if you want, you may delete this now"
